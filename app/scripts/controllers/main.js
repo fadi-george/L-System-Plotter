ea6e5f68c5aa38d21fe099b977ee04506a6f903b
Fadi George <fadii925@gmail.com>
2015-12-24 19:17:58 -0800

    UI Modifications
    UI adjustments

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 
 'use strict';
 
-angular.module('lSystemApp', []).controller('MainCtrl', function($scope) {
+angular.module('lSystemApp', []).controller('MainCtrl', function($scope , $window) {
 
-  $scope.addRule = function(){
 
-  };
+  var w = angular.element($window);
+  console.log(w);
+  // w.resize(function(){
+  //     var h = w.innerHeight();
+  //     var myEl = angular.element( document.querySelector( 'graphInput' ) );
+  // });
 
 });
