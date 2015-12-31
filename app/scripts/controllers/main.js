2122c527abb918a0cfa50ffe43762e588ddcb76f
Fadi George <fadii925@gmail.com>
2015-12-30 18:04:38 -0800

    Updates to ui for adding or deleting rules.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,28 +3,50 @@ var app = angular.module('lSystemApp', ['ngMaterial','ngAnimate','ngCookies','ng
 
 app.controller('MainCtrl', function($scope , $timeout ,  $mdSidenav ) {
 
+  // ------- Variables -----------------------------------------------
 
-  // Variables
-  $scope.numRules = ['1.','2.'];
+  $scope.axiom = '';
+  $scope.gRules = [{'in':'','out':''}];
 
-  $scope.axiom ='';
-
-  // Add rule
-  $scope.addRule = function(){
-    var len = $scope.numRules.length;
-    $scope.numRules.push(len+1+'.');
+  // Save in case of Refresh
+  if( localStorage.axiom ){
+    $scope.axiom = localStorage.axiom;
+  }
+  if( localStorage.gRules ){
+    var arr = JSON.parse( localStorage.gRules );
+    $scope.gRules = arr;
+  }
+  $scope.updateAxiom = function(){
+    localStorage.axiom = $scope.axiom;
+  };
+  $scope.updateRules = function(){
+    var str = angular.toJson($scope.gRules);
+    $scope.validateRules();
+    localStorage.gRules = str;
   };
 
 
-  // Initialization of Canvas Dimensions
+
+  // ------- Add Rule / Delete Rule -----------------------------------------------
+
+  $scope.addRule = function(){
+    ($scope.gRules).push({'in':'','out':''});
+    $scope.updateRules();
+  };
+  $scope.deleteRule = function( idx ){
+    $scope.gRules.splice(idx,1);
+    $scope.updateRules();
+  };
+
+
+  // ------- Initialization of Canvas Dimensions -------------------------------
+
   angular.element(document).ready(function () {
 
     $timeout( function(){
       var w = angular.element( document.querySelector( '#canvasWrapper' ) )[0].clientWidth;
       var h = angular.element( document.querySelector( '#canvasWrapper' ) )[0].clientHeight;
       var c = angular.element( document.querySelector( '#graphCanvas' ) )[0];
-      c.style.width = w + 'px';
-      c.style.height = h + 'px';
       c.width = w;
       c.height = h;
     } , 50);
@@ -32,13 +54,33 @@ app.controller('MainCtrl', function($scope , $timeout ,  $mdSidenav ) {
   });
 
 
-  /*** HELPER FUNCTIONS ****/
+
+  // ------- Helper Functions -----------------------------------------------
+
   $scope.toggleLeft = function() {
     $mdSidenav('left').toggle();
   };
-  $scope.isNavOpen = function(){
-    return $mdSidenav('left').isOpen();
+  $scope.validateRules = function(){
+    let seen = new Set();
+    let pos = 0;
+    var hasDuplicates = $scope.gRules.some(function(currentObject) {
+        pos++;
+        return seen.size === seen.add(currentObject.in).size;
+    });
+    console.log(pos,hasDuplicates,seen);
+    if( hasDuplicates ){
+
+    }
+
   };
+
+
+  // ------ Update Canvas on Run -----------------------------------------------
+  $scope.drawLSystem = function(){
+    var c = angular.element( document.querySelector( '#graphCanvas' ) );
+    var ctx = c.getContext('2d');
+  };
+
 }).directive('resizecanvas', function($window) {
     return function(scope, element, attr) {
         var w = angular.element($window);
@@ -46,13 +88,8 @@ app.controller('MainCtrl', function($scope , $timeout ,  $mdSidenav ) {
             var w = angular.element( document.querySelector( '#canvasWrapper' ) )[0].clientWidth;
             var h = angular.element( document.querySelector( '#canvasWrapper' ) )[0].clientHeight;
 
-            var changes = {
-                width: w + 'px',
-                height: h + 'px',
-            };
             element.width = w;
             element.height = h;
-            element.css(changes);
             scope.$apply();
         });
     };
